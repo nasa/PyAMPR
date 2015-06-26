0df6a0160b0b223f70ffcc3bae036a28baf6bf7d
Timothy Lang <timothy.j.lang@nasa.gov>
2015-06-26 09:42:51 -0500

    More 1.4.1 updates

--- pyampr/__init__.py
+++ pyampr/__init__.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python
 #
-#  The Python Advanced Microwave Precipitation Radiometer Data Toolkit (PyAMPR)
-#  A package to read, analyze, and display AMPR data
-#  
-#  Download AMPR data from http://ghrc.nsstc.nasa.gov.
-#  AMPR brightness temperature data from NASA field projects
-#  are in ASCII format. This python script defines a class that will
-#  read in single file from an individual aircraft flight and pull out
-#  timing, brightness temperatures from each channel, geolocation, and
-#  other information and store them as attributes using numpy
-#  arrays of the appropriate type. The approach is to ingest the entire
-#  file as a text string and then parse and type convert as necessary.
-#  The file is read and the data are populated when the class is
-#  instantiated with the full path and name of an AMPR file.
-#  Numerous visualization methods are provided, including track plots,
-#  strip charts, and Google Earth KMZs. In addition, polarization
-#  deconvolution is available.
+# The Python Advanced Microwave Precipitation Radiometer Data Toolkit (PyAMPR)
+# A package to read, analyze, and display AMPR data
+#
 # 
 __author__ = "Timothy J. Lang"
 
 __email__ = "timothy.j.lang@nasa.gov"
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 __doc__ = """pyampr v%s by %s
 
--- setup.py
+++ setup.py
@@ -13,22 +13,22 @@ try:
 except:
     pass
 
-VERSION='1.4.0'
+VERSION='1.4.1'
 
 DESCRIPTION="The Python Advanced Microwave Precipitation Radiometer Data Toolkit (PyAMPR) - a package to read, analyze, and display AMPR data"
 
-LONG_DESCRIPTION="""Download AMPR data from http://ghrc.nsstc.nasa.gov.
+LONG_DESCRIPTION="""The Advanced Microwave Precipitation Radiometer (AMPR) is an airborne 
+passive microwave radiometer managed by NASA Marshall Space Flight Center.
+Download AMPR data from http://ghrc.nsstc.nasa.gov.
 AMPR brightness temperature data from NASA field projects
-are in ASCII format. This python script defines a class that will 
+are in ASCII or netCDF format. This python script defines a class that will 
 read in single file from an individual aircraft flight and pull out
 timing, brightness temperatures from each channel, geolocation, and
 other information and store them as attributes using numpy 
-arrays of the appropriate type. The approach is to ingest the entire 
-file as a text string and then parse and type convert as necessary.
-The file is read and the data are populated when the class is 
-instantiated with the full path and name of an AMPR file.
+arrays of the appropriate type. The file is read and the data are populated when
+the class is instantiated with the full path and name of an AMPR file.
 Numerous visualization methods are provided, including track plots,
-strip charts, and Google Earth KML/KMZs. In addition, polarization
+strip charts, and Google Earth KMZs. In addition, polarization
 deconvolution is available."""
 
 setup(
